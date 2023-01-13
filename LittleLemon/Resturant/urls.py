cd29638c25b16ffa0b81de8d1a07a6464e32fdc2
Eltoney <aliahmedeltoney@gmail.com>
2023-01-13 23:21:15 +0200

    Add testcases for the project

--- LittleLemon/Resturant/urls.py
+++ LittleLemon/Resturant/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-
+app_name = 'Resturant'
 urlpatterns = [
     # path('', views.index, name='index'),
-    path('menu-items/', views.MenuItemView.as_view()),
+    path('menu-items/', views.MenuItemView.as_view(), name='all_items'),
     path('menu-items/<int:pk>', views.SingleMenuItemView.as_view())
 ]
